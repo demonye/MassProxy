976964e1d65183f46bc908dd2a588216ea866e63
Eric Ye <hongqiang.ye@gmail.com>
2017-06-28 01:08:23 +1200

    Refactored main file with class, docstring

--- proxy_serv.py
+++ proxy_serv.py
@@ -11,10 +11,6 @@ from multiprocessing import pool
 import ps_util
 import ps_struct
 
-conn_list = ps_struct.ConnList()
-monitor_lock = threading.Lock()
-ep = select.epoll()
-
 _EPOLLRDHUP = 0x2000
 READ = select.EPOLLIN | _EPOLLRDHUP
 WRITE = select.EPOLLOUT
@@ -24,99 +20,111 @@ WRITEMODE = WRITE | ERROR | select.EPOLLET
 BUFSIZE = 4096
 
 
-def read_header(fd):
+class Proxy(object):
     """
-    Given the socket descriptor, get ip and port from data header
-
-    :param fd: socket descriptor to read data header
-    :returns: IP address, port of the server to connect
+    Proxy class that starts a thread to accept connection
+    and put working threads in pool
     """
-    _ip = fd.recv(4)
-    ip = socket.inet_ntoa(_ip)
-    _port = fd.recv(2)
-    port = struct.unpack('>H', _port)[0]
-    return ip, port
 
+    def __init__(self, sock):
+        """
+        Initialize data
 
-def add_monitor(fd):
-    """
-    Get server information from the given socket descriptor (client),
-    connect and then start monitoring both client and server descriptors.
+        :param sock: socket object bound to host/port
+        """
+        self.conn_list = ps_struct.ConnList()
+        self.monitor_lock = threading.Lock()
+        self.ep = select.epoll()
+        self.sock = sock
 
-    :param fd: socket descriptor to be monitored
-    """
-    host, port = read_header(fd)
-    opfd = ps_util.connect_to(host, port)
-    with monitor_lock:
-        if opfd:
-            conn_list[fd.fileno()] = ps_struct.ConnItem(fd, opfd, 'C')
-            fd.setblocking(0)
-            opfd.setblocking(0)
-            ep.register(fd.fileno(), READMODE)
-            ep.register(opfd.fileno(), READMODE)
-        else:
-            fd.close()
+    def read_header(self, fd):
+        """
+        Given the socket descriptor, get ip and port from data header
 
+        :param fd: socket descriptor to read data header
+        :return: IP address, port of the server to connect
+        """
+        _ip = fd.recv(4)
+        ip = socket.inet_ntoa(_ip)
+        _port = fd.recv(2)
+        port = struct.unpack('>H', _port)[0]
+        return ip, port
 
-def remove_monitor(fd):
-    """
-    Given client or server descriptor, stop monitoring both sides
+    def add_monitor(self, fd):
+        """
+        Get server information from the given socket descriptor (client),
+        connect and then start monitoring both client and server descriptors.
 
-    :param fd: Client or server socket descriptor
-    """
-    with monitor_lock:
-        item = conn_list[fd.fileno()]
-        opfd = item.opfd
-        ep.unregister(opfd.fileno())
-        ep.unregister(fd.fileno())
-        del conn_list[fd]
-        fd.close()
-        opfd.close()
-
-
-def accept_process(sock):
-    """
-    Accept the inbound connection and monitor the socket descriptor
-
-    :param sock: the socket object which has been bound
-    :raises: raise exceptiion for socket error,
-             if being interrupted by signal, ignore and continue
-    """
-    while True:
-        try:
-            fd, addr = sock.accept()
-            add_monitor(fd)
-        except socket.error as ex:
-            if ex.args[0] != errno.EINTR:
-                raise
-
-
-def read_handler(fd_):
-    """
-    Given client or server descriptor, stop monitoring both sides
-
-    :param fd_: Client or server socket descriptor
-    :raises Exception: raise error if not EWOULDBLOCK and EAGAIN
-    """
-    item = conn_list.get(fd_, None)
-    fd = item.fd
-    if item is None:
-        return
-    opfd = item.opfd
-    while True:
-        data = ""
-        try:
-            data = fd.recv(BUFSIZE)
-            if len(data) == 0:
-                remove_monitor(fd)
-                break
-            opfd.sendall(data)
-            ep.register(fd_, READMODE)
-        except socket.error as e:
-            if e.args[0] in (errno.EWOULDBLOCK, errno.EAGAIN):
-                break
+        :param fd: socket descriptor to be monitored
+        """
+        host, port = self.read_header(fd)
+        opfd = ps_util.connect_to(host, port)
+        with self.monitor_lock:
+            if opfd:
+                self.conn_list[fd.fileno()] = ps_struct.ConnItem(fd, opfd, 'C')
+                fd.setblocking(0)
+                opfd.setblocking(0)
+                self.ep.register(fd.fileno(), READMODE)
+                self.ep.register(opfd.fileno(), READMODE)
             else:
-                raise
+                fd.close()
+
+    def remove_monitor(self, fd):
+        """
+        Given client or server descriptor, stop monitoring both sides
+
+        :param fd: Client or server socket descriptor
+        """
+        with self.monitor_lock:
+            item = self.conn_list[fd.fileno()]
+            opfd = item.opfd
+            self.ep.unregister(opfd.fileno())
+            self.ep.unregister(fd.fileno())
+            del self.conn_list[fd]
+            fd.close()
+            opfd.close()
+
+    def accept_process(self):
+        """
+        Accept the inbound connection and monitor the socket descriptor
+
+        :raises: raise exceptiion for socket error,
+                if being interrupted by signal, ignore and continue
+        """
+        while True:
+            try:
+                fd, addr = self.sock.accept()
+                self.add_monitor(fd)
+            except socket.error as ex:
+                if ex.args[0] != errno.EINTR:
+                    raise
+
+    def read_handler(self, fd):
+        """
+        Given client or server descriptor, stop monitoring both sides
+
+        :param fd: Client or server socket descriptor
+        :raises Exception: raise error if not EWOULDBLOCK and EAGAIN
+        """
+        item = self.conn_list.get(fd, None)
+        _fd = item.fd
+        if item is None:
+            return
+        opfd = item.opfd
+        while True:
+            data = ""
+            try:
+                data = _fd.recv(BUFSIZE)
+                if len(data) == 0:
+                    self.remove_monitor(_fd)
+                    break
+                opfd.sendall(data)
+                self.ep.register(fd, READMODE)
+            except socket.error as e:
+                if e.args[0] in (errno.EWOULDBLOCK, errno.EAGAIN):
+                    break
+                else:
+                    raise
 
 
 def main():
@@ -146,15 +154,16 @@ def main():
     thds, tmout, maxevts = args.thread, args.timeout, args.maxevents
 
     sock = ps_util.bind_socket(args.port)
-    pool = pool.ThreadPool(thds)
+    pool_ = pool.ThreadPool(thds)
 
-    a_proc = threading.Thread(target=accept_process, args=(sock,))
+    proxy = Proxy(sock)
+    a_proc = threading.Thread(target=proxy.accept_process, args=[sock])
     a_proc.start()
     while True:
-        for (fd, evts) in ep.poll(tmout, maxevts):
+        for (fd, evts) in proxy.ep.poll(tmout, maxevts):
             if evts & (READ | ERROR):
-                ep.unregister(fd)
-                pool.apply(read_handler, [fd])
+                proxy.ep.unregister(fd)
+                pool_.apply(proxy.read_handler, [fd])
 
 
 if __name__ == "__main__":
--- ps_struct.py
+++ ps_struct.py
@@ -6,12 +6,20 @@ class ConnItem(object):
     _types = ('C', 'S')
 
     def __init__(self, fd, opfd, ctype):
+        """
+        Create connection item and validate value
+
+        :param fd: socket descriptor
+        :param opfd: socket ddescriptor of the other peer
+        :param ctype: connection type (C)lient or (S)erver
+        """
         self.fd = fd
         self.opfd = opfd
         self.ctype = ctype
         self.valid_value()
 
     def valid_value(self):
+        """Validate socket descriptors"""
         if type(self.fd) != socket.socket:
             raise ValueError
         if type(self.opfd) != socket.socket:
@@ -20,20 +28,36 @@ class ConnItem(object):
             raise ValueError
 
     def is_client(self):
+        """Check if the type is (C)lient"""
         return self.ctype == 'C'
 
 
 class ConnList(dict):
+    """List of connection items, inherited from dictionary"""
+
     _lock = threading.Lock()
 
-    def revert_type(self, t):
-        if t == ConnItem._types[0]:
-            return ConnItem._types[1]
-        if t == ConnList._types[1]:
-            return ConnItem._types[0]
+    def revert_type(self, tp):
+        """
+        Return the opposite type
+
+        :param tp: connection type
+        :return: the opposite type
+        """
+        try:
+            types = ConnItem._types
+            return types[1 - types.index(tp)]
+        except ValueError:
+            pass
         return None
 
     def __setitem__(self, k, item):
+        """
+        Set item value with the key k, set the related item as well
+
+        :param k: the key
+        :param item: the value
+        """
         item.valid_value()
         fd, opfd, ctype = item.fd, item.opfd, item.ctype
         rt = self.revert_type(ctype)
@@ -46,13 +70,15 @@ class ConnList(dict):
             )
 
     def __delitem__(self, k):
+        """
+        Delete k and the related item from the list
+
+        :param k: the key
+        """
         item = self.get(k, None)
         if item is None:
             return
-        print "item", item
         opfd = item.opfd
-        print "opfd", opfd.fileno()
-        print "opfd.fileno()", self.get(opfd.fileno(), None)
         with self._lock:
             super(ConnList, self).__delitem__(k)
             super(ConnList, self).__delitem__(opfd.fileno())
